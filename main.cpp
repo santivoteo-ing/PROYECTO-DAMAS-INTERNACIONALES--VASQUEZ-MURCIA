2b50a36b4f45199760160283f90d0ff0030f102a
santivoteo-ing <santivoteo@gmail.com>
2025-11-12 07:30:38 -0500

    Change game title in main menu

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
     while (1) {
-        printf("\n--- DAMAS INTERNACIONALES - MINIMO ---\n");
+        printf("\n--- DAMAS---\n");
         printf("1) Iniciar partida\n");
         printf("2) Ver reglas\n");
         printf("3) Salir\n");
